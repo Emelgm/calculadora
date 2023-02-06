b38ce800f01b9fb5a9b0a6592766e7401d76b6b4
Peter Cerpa <cerpal004@gmail.com>
2023-02-06 11:22:42 -0500

    Cambios en los valores de los parametros

--- main.py
+++ main.py
@@ -26,21 +26,21 @@ while opcion != 'x' or opcion != 'X':
         if opcion_2 == 'a' or opcion_2 == 'A':
             print("\nCircuitos en serie")
             result = 0
-            cantidad = int(input("\nCantidad de R a calcular: "))
+            cantidad = int(input("\nCantidad de Resistencias a calcular: "))
             for i in range(1, cantidad + 1):
-                r = float(input(f"Valor de R{i} en kΩ: "))
+                r = float(input(f"Valor de Resistencia {i} en kΩ: "))
                 result += r
-            print(f"\nLa R resultante es: {round(result,2)} kΩ")
+            print(f"\nLa Resistencia resultante es: {round(result,2)} kΩ")
         # cricuito en paralelo
         elif opcion_2 == 'b' or opcion_2 == 'B':
             print("\nCircuitos en paralelo")
             result = 0
-            cantidad = int(input("\nCantidad de R a calcular: "))
+            cantidad = int(input("\nCantidad de Resistencias a calcular: "))
             for i in range(1, cantidad + 1):
-                r = float(input(f"Valor de R{i} en kΩ: "))
+                r = float(input(f"Valor de Resistencia {i} en kΩ: "))
                 result = result+(1/r)
             rtotal = 1/result
-            print(f"\nLa R resultante es: {round(rtotal,2)} kΩ")
+            print(f"\nLa Resistencia resultante es: {round(rtotal,2)} kΩ")
         else:
             print("\n¡¡ OPCIÓN EQUIVOCADA !!")
             opcion = 'a'
@@ -63,20 +63,20 @@ while opcion != 'x' or opcion != 'X':
         omh = input("[A] - Voltaje\n[B] - Corriente\n[C] - Resistencia\n: ")
         if omh == 'a' or omh == 'A':
             print("\nVoltaje ----> V = I * R")
-            corriente = float(input("Valor de I en mA: "))
-            resistencia = float(input("Valor de R en kΩ: "))
+            corriente = float(input("Valor de Corriente en mA: "))
+            resistencia = float(input("Valor de Resistencia en kΩ: "))
             voltaje = corriente * resistencia
             print(f"Voltaje = {round(voltaje,3)}")
         elif omh == 'b' or omh == 'B':
             print("\nCorriente ---> I = V / R")
-            voltaje = float(input("Valor de V: "))
-            resistencia = float(input("Valor de R en kΩ: "))
+            voltaje = float(input("Valor de Voltaje: "))
+            resistencia = float(input("Valor de Resistencia en kΩ: "))
             corriente = voltaje / resistencia
             print(f"Corriente = {round(corriente, 3)} mA")
         elif omh == 'c' or omh == 'C':
             print("\nResistencia ----> R = V / I")
-            voltaje = float(input("Valor de V: "))
-            corriente = float(input("Valor de I en mA: "))
+            voltaje = float(input("Valor de Voltaje: "))
+            corriente = float(input("Valor de Corriente en mA: "))
             resistencia = voltaje / corriente
             print(f"Resistencia = {round(resistencia, 2)} kΩ")
         else:
@@ -99,20 +99,20 @@ while opcion != 'x' or opcion != 'X':
         param = input("[A] - Longitud\n[B] - Frecuencia\n[C] - Velocidad\n:")
         if param == 'a' or param == 'A':
             print("\nFórmula ----> λ = v / f\n")
-            velocidad = float(input("Valor de V m/s: "))
-            frecuencia = float(input("Valor de F en Hz: "))
+            velocidad = float(input("Valor de Velocidad m/s: "))
+            frecuencia = float(input("Valor de Frecuencia en Hz: "))
             longitud = velocidad/frecuencia
             print(f"\nLa longitud de onda es λ = {round(longitud,2)} m")
         elif param == 'b' or param == 'B':
             print("\nFórmula ----> f = v / λ\n")
-            velocidad = float(input("Valor de V m/s: "))
-            longitud = float(input("Valor de λ en m: "))
+            velocidad = float(input("Valor de Velocidad m/s: "))
+            longitud = float(input("Valor de Longitud (λ) en m: "))
             frecuencia = velocidad/longitud
             print(f"\nLa frecuencia de la onda es f = {round(frecuencia,2)} Hz")
         elif param == 'c' or param == 'C':
             print("\nFórmula ----> v = λ * f\n")
-            frecuencia = float(input("Valor de F en Hz: "))
-            longitud = float(input("Valor de λ en m: "))
+            frecuencia = float(input("Valor de FrecuenciaF en Hz: "))
+            longitud = float(input("Valor de Longitud (λ) en m: "))
             velocidad = frecuencia*longitud
             print(f"\nLa velocidad de la onda es v = {round(velocidad,2)} m/s")
         else:
